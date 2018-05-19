65d13e75d7191c72f5ea45ad6a5a4881d178ebc8
David Timm <timmmeister@gmail.com>
2018-05-18 18:02:50 -0600

    Strip ginkgo tests

--- scraper/scraper_test.go
+++ scraper/scraper_test.go
@@ -3,18 +3,16 @@ package scraper_test
 import (
 	"github.com/dtimm/go-twitter-scraper/scraper"
 
-	. "github.com/onsi/ginkgo"
+	"testing"
+
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("Scraper", func() {
-	Context("when called on a user", func() {
-		It("returns all of their tweets", func() {
-			scrape := scraper.New()
-			tweets, err := scrape.User("vietarmis")
-			Expect(err).ToNot(HaveOccurred())
+func TestScraper(t *testing.T) {
+	g := NewGomegaWithT(t)
+	scrape := scraper.New()
+	tweets, err := scrape.User("vietarmis")
+	g.Expect(err).ToNot(HaveOccurred())
 
-			Expect(tweets).To(HaveLen(49))
-		})
-	})
-})
+	g.Expect(tweets).To(HaveLen(49))
+}
